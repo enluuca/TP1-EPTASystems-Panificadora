1bbf322458d0d36b049caa7f3cae5e15688ca5f6
Enzo <giangreco.en@gmail.com>
2026-04-16 20:00:40 -0300

    Estructura proyecto BackEnd

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Configuración del Motor de Plantillas (Pug)
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.static(path.join(__dirname, 'public'))); // Archivos estáticos
+app.use(express.json()); // Para recibir JSON
+app.use(express.urlencoded({ extended: true })); // Para recibir datos de formularios
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.render('index', { title: 'La Espiga de Oro - Sistema de Gestión' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
