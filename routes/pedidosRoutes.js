841dc3fe01cfed3ad01cec186eb8b22bc5d16727
Poff93 <pablo-93@hotmail.com.ar>
2026-05-21 09:27:40 -0300

    Configuración de enpoints

--- routes/pedidosRoutes.js
+++ routes/pedidosRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const pedidosController = require('../controllers/pedidosController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+// Aplicamos barrera de seguridad con JWT para toda la gestión de pedidos
+router.use(authMiddleware);
 
 // Rutas GET (Lectura)
 router.get('/', pedidosController.getPedidos);
